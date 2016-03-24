959459d785c4f7e7d52c3062d7b81907e21e8452
fstojkovic <filip.cofi.stojkovic@gmail.com>
2016-03-24 19:13:00 +0100

    Napisana JavaDoc dokumentacija za klasu Twitter, njene atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,30 +1,83 @@
 
 package com.twitter.poruke;
 
+/**
+ * Ova klasa implementira ime korisnika i njegovu poruku
+ * 
+ * @author Filip Stojkovic
+ * @version 1.0
+ */
 public class TwitterPoruka {
+	/**
+	 * Ime korisnika tipa string
+	 */
 	private String korisnik;
+	/**
+	 * Poruka korisnika tipa string
+	 */
 	private String poruka;
 
+	/**
+	 * Metoda vraca ime korisnika
+	 * 
+	 * @return ime korisnika
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost za atribut korisnik
+	 * 
+	 * @param korisnik
+	 *            ime korisnika
+	 * 
+	 * @throws java.lang.RuntimeException
+	 *             ako je unet ime:
+	 *             <ul>
+	 *             <li>null</li>
+	 *             <li>prazan string</li>
+	 *             </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Metoda vraca poruku korisnika
+	 * 
+	 * @return poruku korisnika
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+	/**
+	 * Metoda postavlja novu vrednost za atribut poruka
+	 * 
+	 * @param poruka
+	 *            
+	 * 
+	 * @throws java.lang.RuntimeException
+	 *             ako je uneta poruka:
+	 *             <ul>
+	 *             <li>null</li>
+	 *             <li>duzine manje od 140 karaktera</li>
+	 *             </ul>
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
 
+	/**
+	 * 
+	 * Metoda vraca string sa podacima o korisniku i njegovoj poruku
+	 * 
+	 * @return Ime korisnika i njegovu poruku
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
